Fixed bug in calculation of nearest point to ellipse.

tests/test_ellipse.py
@@ -38,7 +38,7 @@ def test_rotation_constructor():
     assert 0.5 < ell.y_size < 0.9
 
 
-def test_circle():
+def test_circles():
     for angle in np.random.rand(20):
         rot = rotation_matrix(angle, 2)
         ell = EllipseWrap.create_from_rot_radii(1.5, 1.5, rot)
@@ -50,5 +50,49 @@ def test_circle():
         for x, y in np.random.rand(200, 2):
             expect = x*x + y*y < .9**2
             assert ell.line_intersects(x, y) == expect
-            expect = (x,y) if expect else .9*np.array([x, y]) / np.sqrt(x*x+y*y)
+            expect = (x, y) if expect else .9 * np.array([x, y]) / np.sqrt(x*x + y*y)
             assert ell.nearest_to_line(x, y)[:2] == approx(expect, abs=1e-6)
+
+
+def test_ellipses():
+    for angle, r1, r2 in np.random.rand(20, 3):
+        angle *= 360.
+        r1 = 2*r1 + .2
+        r2 = 2*r2 + .2
+        rot = rotation_matrix(angle, 2)
+        assert rot.T @ rot == approx(np.eye(3), rel=1e-12)
+        ell = EllipseWrap.create_from_rot_radii(r1, r2, rot)
+        assert np.linalg.norm(ell.e1) == approx(r1, rel=1e-9)
+        assert np.linalg.norm(ell.e2) == approx(r2, rel=1e-9)
+        # Check reverse transform
+        assert rot.T @ np.row_stack(ell.e1) == approx([r1, 0., 0.])
+        assert rot.T @ np.row_stack(ell.e2) == approx([0., r2, 0.])
+        # Area is conserved under rotation
+        area = 2 * np.pi * np.linalg.norm(ell.e1) * np.linalg.norm(ell.e2)
+        assert area == approx(2 * np.pi * r1 * r2, rel=1e-9)
+        # Line intersection and nearest-point check
+        for x, y in 6 * np.random.rand(200, 2) - 3:
+            xp, yp, zp = np.ravel(rot.T @ np.row_stack([x, y, 0.]))
+            assert zp == 0.
+            assert xp**2 + yp**2 == approx(x**2 + y**2, 1e-12)
+            dist = (xp / r1)**2 + (yp / r2)**2
+            assert ell.line_intersects(x, y) == (dist < 1)
+            nearest = ell.nearest_to_line(x, y)
+            if dist < 1:
+                assert nearest == approx([x, y, 0.], 1e-9)
+            else:
+                # Point should be on the ellipse boundary
+                assert nearest[2] == approx(0., rel=1e-12)
+                nearestp = np.ravel(rot.T @ np.row_stack(nearest))
+                assert nearestp[2] == approx(0., rel=1e-12)
+                dist = (nearestp[0] / r1)**2 + (nearestp[1] / r2)**2
+                assert dist == approx(1, rel=1e-12)
+                # Point should be closer to the target than the origin
+                displacement = np.array([x - nearest[0], y - nearest[1]])
+                assert np.linalg.norm(displacement) < np.linalg.norm([x, y])
+                # Normal vector at nearest point should point from it to the target point
+                displacement = np.array([xp - nearestp[0], yp - nearestp[1]])
+                normal_vec = np.array([nearestp[0] / (r1**2), nearestp[1] / (r2**2)])
+                normal_vec /= np.linalg.norm(normal_vec)
+                displacement /= np.linalg.norm(displacement)
+                assert normal_vec == approx(displacement, rel=1e-9)